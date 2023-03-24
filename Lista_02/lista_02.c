93b476a5ee1591d4086f425808421a5b45383a4a
Samuel Thomas <samuel.thomas1109@outlook.com>
2023-03-23 22:16:07 -0300

    Aula dia 23/03/2023

--- Lista_02/lista_02.c
+++ Lista_02/lista_02.c
@@ -99,24 +99,59 @@ return;
 
 //6.  Escreva um algoritmo que leia três valores inteiros e diferentes e mostre-os em ordem decrescente. 
     void questao06() {
-        int a, b, c;
-        int r1, r2, r3;
+        int a, b, c, temp;
 
-        printf("Insira 3 números inteiros e diferentes: ");
+        printf("Insira 3 números (separado por espaço) inteiros e diferentes: ");
         scanf("%i %i %i", &a, &b, &c);
-        printf("%i %i %i", a, b, c);
 
-        if(a > b, a > c) {
-            a = r3;
+        if (a < b) {
+            temp = a;
+            a = b;
+            b = temp;
         };
-        if(a > b, a < c) {
-            
+        if (b < c) {
+            temp = b;
+            b = c;
+            c = temp;
+        };
+        if (b > a) {
+            temp = b;
+            b = a;
+            a = temp;
         }
+
+        printf("Números em ordem: %i -> %i -> %i", a, b, c);
+
 return;
 }
 
-//
+//7. Tendo como dados de entrada a altura e o sexo de uma pessoa, construa um algoritmo que calcule seu peso ideal, utilizando as seguintes fórmulas: 
+//para homens: (72.7 * h) - 58; 
+//para mulheres: (62.1 * h) - 44.7. 
     void questao07() {
+        float h, pesoIdeal;
+        char sexo;
+
+        printf("Insira sua altura: ");
+        scanf("%f", &h);
+        
+        getchar(); // limpa o buffer de entrada. Ao chamar a função getchar(), você está lendo um caractere do buffer de entrada, o que remove o caractere residual que poderia estar lá após a entrada da altura.
+
+        printf("Selecione seu sexo (M/F): ");
+        //fflush(stdin); Limpa todo o buffer
+        scanf("%c", &sexo);
+        printf("O valor lido para sexo é '%c'\n", sexo);
+
+        if (sexo == 'M' || sexo == 'm') {
+            pesoIdeal = (72.7 * h) - 58;
+            printf("Você é homem e seu peso ideal é: %.2f Kg", pesoIdeal);
+        }
+        else if (sexo == 'F' || sexo == 'f') {
+            pesoIdeal = (62.1 * h) - 44.7;
+            printf("Você é mulher e seu peso ideal é: %.2f Kg", pesoIdeal);
+        } else {
+            printf("Sexo desconhecido!");
+        }
 
 return;
 }
@@ -127,8 +162,34 @@ return;
 return;
 }
 
-//
+//Escreva um algoritmo que leia o número de identificação do aluno, 3 notas obtidas e calcule a média aritmé-tica obtida pelo aluno. Ao final informe se o aluno foi aprovado direto, ficou em recuperação ou foi reprovado por nota.
     void questao09() {
+        float nota1, nota2, nota3, media;
+        int idAluno;
+
+        //Input das notas
+        printf("Insira o número de identificação do aluno: ");
+        scanf("%i", &idAluno);
+        printf("Insira o valor da primeira nota: ");
+        scanf("%f", &nota1);
+        printf("Insira o valor da segunda nota: ");
+        scanf("%f", &nota2);
+        printf("Insira o valor da terceira nota: ");
+        scanf("%f", &nota3);
+
+        //Cálculo da média
+        media = (nota1 + nota2 + nota3)/3;
+
+        //Verificador da média
+        if (media >= 7) {
+            printf("Aluno nº %.0i, não fez mais que sua obrigação!", idAluno); 
+            } else {
+                if (media >= 4, media < 7) {
+                    printf("Aluno nº %.0i, você está de recuperação!", idAluno); 
+                } else {
+                    printf("Aluno nº %.0i, você está reprovado, noob!!!", idAluno); 
+                }
+            };
 
 return;
 }
@@ -181,6 +242,35 @@ return;
 return;
 }
 
+//Switch Case
+    void questao18() {
+        int num = 0;
+
+        printf("insira um número de 0 a 3: ");
+        scanf("%d", &num);
+
+        switch (num)
+        {
+        case 0:
+            printf("Zero");
+            break;
+        case 1:
+            printf("Um");
+            break;
+        case 2:
+            printf("Dois");
+            break;
+        case 3:
+            printf("Três");
+            break;
+
+        default:
+            printf("What?");
+            break;
+        }
+
+return;
+}
 
 
 int main() {
@@ -189,9 +279,9 @@ int main() {
     while (1)
     {
         int i, selecao;    
-        void (*questao[17])() = {questao01, questao02, questao03, questao04, questao05, questao06, questao07, questao08, questao09, questao10, questao11, questao12, questao13, questao14, questao15, questao16, questao17};
+        void (*questao[18])() = {questao01, questao02, questao03, questao04, questao05, questao06, questao07, questao08, questao09, questao10, questao11, questao12, questao13, questao14, questao15, questao16, questao17, questao18};
 
-        printf ("\nDigite o número da questão (de 1 a 17): ");
+        printf ("\nDigite o número da questão (de 1 a 18): ");
         scanf("%d", &selecao);
 
         for (i = 0; i < 40; i++) {
